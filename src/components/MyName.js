fb9aef189cf84c154a4c40b2bae783080e195f6b
Tenzin <tenthinten@gmail.com>
2022-01-31 13:41:44 +0530

    css for aboutme

--- src/components/MyName.js
+++ src/components/MyName.js
@@ -4,6 +4,7 @@ export default function MyName() {
     return (
         <div className='myname'>
             <h1>Tenzin Thinley</h1>
+            <h4>Frontend Developer</h4>
         </div>
     )
 }
