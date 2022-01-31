758efad4f76ca80484cc39e43ad7da16da463eed
Tenzin <tenthinten@gmail.com>
2022-01-31 20:56:52 +0530

    add github linke and email

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function AboutMe() {
     return (
         <div className='aboutme'>
+            <br />
            <p>Hello, I am frontend developer, Building state of art user friendly Web app is a passion of mine. <br/>
                 Strong understanding and proficiency in ReactJS, JavaScript, HTML, CSS, SCSS, Django, SQL <br/>
                 and Python. Professional attitude with strong work ethics and always eager to learn new things, <br/> 
--- src/components/MyName.js
+++ src/components/MyName.js
@@ -3,9 +3,18 @@ import React from 'react';
 export default function MyName() {
     return (
         <div className='myname'>
+            <div className='name'>
             <h1>Tenzin Thinley</h1>
+            </div>
+            <div className='jobtitle'>
             <h5>Frontend Developer</h5>
-            <hr />
+            </div>
+
+            <div className='github'>
+                <p><a href="https://github.com/tenthin">github</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email: tenthinten@gmail.com</p>
+            </div>
+     
+
         </div>
     )
 }
